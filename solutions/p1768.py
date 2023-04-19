8a54bb16e1fcfe5a8b58cfcf5dee41c7c473262d
philipsoitu <95496892+philipsoitu@users.noreply.github.com>
2023-04-19 10:04:36 -0400

    april 18 problem

--- solutions/p1768.py
+++ solutions/p1768.py
@@ -1,3 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        return "".join(i[0]+i[1] for i in zip(word1,word2)) + word1[len(word2):] + word2[len(word1):]
+        ans = ""
+        shortest_word = min(len(word1), len(word2))
+        for i in range(shortest_word):
+            ans+=word1[i] + word2[i]
+        ans+= word1[shortest_word:] + word2[shortest_word:]
+        return ans
+
+
+print(Solution.mergeAlternately(Solution, "", "123"))
+        
